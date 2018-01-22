e3f99c917b5cc63c7f3dd3cd59e5421cfc3f86d2
AraslanovAA <araslanov.anton.p@gmail.com>
2018-01-22 16:24:45 +0300

    create console

---
+++ calc220118/calc220118/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc220118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = args[0];
+            string num1 = args[1];
+            string num2 = args[2];
+            double num1_d, num2_d;
+            double.TryParse(num1, out num1_d);
+            double.TryParse(num2, out num2_d);
+            if (operation == "summ")
+            {
+                Console.WriteLine(num2_d + num1_d);
+            }
+        }
+    }
+}
